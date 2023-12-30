fad833ee6bd49ed3aee47fdb8a01aa8808bd772c
Nat Tuck <nat@ferrus.net>
2023-12-30 13:53:59 -0500

    before create users

---
+++ daemon/isolabd/lib/isolabd.ex
@@ -0,0 +1,18 @@
+defmodule Isolabd do
+  @moduledoc """
+  Documentation for `Isolabd`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Isolabd.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ daemon/isolabd/test/isolabd_test.exs
@@ -0,0 +1,8 @@
+defmodule IsolabdTest do
+  use ExUnit.Case
+  doctest Isolabd
+
+  test "greets the world" do
+    assert Isolabd.hello() == :world
+  end
+end
